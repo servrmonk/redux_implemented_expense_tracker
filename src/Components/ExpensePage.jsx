f32262a382f508080f5ab60bc8bf6a42ebbcea97
Raushan <roushankumarsingh2018@gmail.com>
2024-01-20 06:24:40 +0530

    Premium button add if expense is greater than 10k

--- src/Components/ExpensePage.jsx
+++ src/Components/ExpensePage.jsx
@@ -180,6 +180,12 @@ export default function ExpensePage() {
             {sumTotalExpense(Expenses)}
           </p>
         </div>
+
+        {sumTotalExpense(Expenses) > 10000 &&
+        <button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2  h-10 mt-8 mx-2 mb-2">Premium</button>
+        }
+        
+
       </div>
       {/* CURRENT EXPENSE LEFT DOWN  */}
 
